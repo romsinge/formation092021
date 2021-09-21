b6012aad87220f5c780bf788c046eb41f8235f95
rsignes <romain.signes@gmail.com>
2021-09-21 16:57:28 +0200

    exercice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,21 +25,25 @@ export class AppComponent {
 
   ponies: Poney[] = [
     {
+      id: "1",
       name: "Romain",
       image: "https://ng-ponyracer.ninja-squad.com/assets/images/pony-green-running.gif",
       color: "gold"
     },
     {
+      id: "2",
       name: "Yoann",
       image: "https://ng-ponyracer.ninja-squad.com/assets/images/pony-blue-running.gif",
       color: "rebeccapurple"
     },
     {
+      id: "3",
       name: "Emile",
       image: "https://ng-ponyracer.ninja-squad.com/assets/images/pony-orange-running.gif",
       color: "lavender"
     },
     {
+      id: "4",
       name: "Bertrand",
       image: "https://ng-ponyracer.ninja-squad.com/assets/images/pony-purple-running.gif",
       color: "orangered"
